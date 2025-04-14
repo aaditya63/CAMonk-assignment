0ae1da1aefd24407cb46854a8e15e419207210d6
Aadita Sah <aadityasah13@gmail.com>
2025-04-14 16:23:24 +0530

    deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Route, Routes } from "react-router-dom"
-import Home from "./Pages/home"
+import Home from "./Pages/Home"
 import Test from "./Pages/Test"
 import Result from "./Pages/Result"
 
